32a097c463ca2a99feb29727cf43b944f28dff05
zhaojunjie.passaway <zhaojunjie.passaway@bytedance.com>
2021-10-08 10:07:34 +0800

    ..

--- app/src/main/java/com/example/learncoroutines/code/FirstLearn.kt
+++ app/src/main/java/com/example/learncoroutines/code/FirstLearn.kt
@@ -1,6 +1,5 @@
 package com.example.learncoroutines.code
 
-import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.MainScope
@@ -27,19 +26,39 @@ fun main() {
 //        }
 //    val end2 = System.currentTimeMillis()
 //    println("\n线程"+(end2 - start2))
+
     println("main start")
-    GlobalScope.launch {
-        val result = getString()
+    GlobalScope.launch(Dispatchers.Default) {
+        //delay(1000)
+        println("GlobalScope main start")
+        var str = withContext(Dispatchers.IO){
+            delay(2000)
+            getString("withContext")
+        }
+        println(str)
+        launch {
+            delay(1500)
+            getString2("launch")
+        }
+
         println("wait")
-        println(result)
+        delay(1000)
+        println("jj")
     }
 
-    Thread.sleep(3000)
     println("main end")
-
+    Thread.sleep(5500)
+    println("main over")
 }
-suspend fun getString(): String {
-    delay(2000)
-    println("delay 2000 then return response")
+
+suspend fun getString2(string: String): String {
+    println("${string} delay 1500 then return response")
     return "resposne data"
 }
+suspend fun getString(string: String): String {
+    println("${string} delay 2000 then return response")
+    return "resposne data"
+}
+
+fun getUser(string: String): String = string
+
